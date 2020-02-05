776fb58f2065a02d3c03aef3005ec83e1cbebf76
Jmjimmy20 <you@example.com>
2020-02-04 23:54:35 -0500

    2 test

--- Lab2/IMMORTALS/src/test/java/edu/eci/arsw/InmmortalTest.java
+++ Lab2/IMMORTALS/src/test/java/edu/eci/arsw/InmmortalTest.java
@@ -42,4 +42,68 @@ public class InmmortalTest{
     }
 
 
+    /*
+    @Test
+    public void testContinueButton(){
+        immortals = frame.setupInmortals();
+        int vida = 10000;
+        boolean isChange = false;
+        if (immortals != null) {
+            for (Immortal im : immortals) {
+                im.start();
+            }
+
+        }
+        for (Immortal im : immortals) {
+            im.cambiarVida(vida);
+            if(im.getHealth() == vida) isChange = true;
+            System.out.println(im.getHealth() + " bla" );
+        }
+        System.out.println(isChange);
+        assertTrue(isChange);
+    }*/
+
+    @Test
+    public void testStopButton(){
+        immortals = frame.setupInmortals();
+        if (immortals != null) {
+            for (Immortal im : immortals) {
+                im.start();
+            }
+
+        }
+        for (Immortal im : immortals) {
+            im.paraDePelear();
+        }
+
+        for (Immortal im : immortals) {
+            im.detenerThread();
+            isPause =  im.estaPeleando; 
+        }
+        assertFalse(isPause);
+    }
+
+    @Test
+    public  void testContinueButton(){
+        immortals = frame.setupInmortals();
+        if (immortals != null) {
+            for (Immortal im : immortals) {
+                im.start();
+            }
+
+        }
+        for (Immortal im : immortals) {
+            im.paraDePelear();
+            isPause =  im.estaPeleando; 
+        }
+
+        for (Immortal im : immortals) {
+            im.siguePeleando();
+            isPause =  im.estaPeleando; 
+        }
+        
+        assertTrue(isPause);
+    }
+
+
 }
